69969005ac7cfc95cf189cdcc9de77f107e14947
Dhanushka Kumarage <kosalamoon@gmail.com>
2023-03-30 19:59:43 +0530

    theme & font color

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Routes from './routes';
+import {ConfigProvider} from 'antd';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <>
-    <Routes />
+    <ConfigProvider
+      theme={{
+        token: {
+          colorText: '#444444',
+          colorPrimary: '#1e88e5',
+        },
+      }}>
+      <Routes />
+    </ConfigProvider>
   </>,
 );
